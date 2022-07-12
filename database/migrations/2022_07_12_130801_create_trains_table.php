af8fab1b17291a3143fd7d04b489102487b24080
BeainaT <tania.df@hotmail.it>
2022-07-12 15:39:56 +0200

    created table trains+migration

---
+++ database/migrations/2022_07_12_130801_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->unsignedTinyInteger('train_code');
+            $table->unsignedTinyInteger('wagon');
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
